ae9640d81acfd5f87d0871c91284300e8cf68525
SmallMonkey-ai <2641973190@qq.com>
2022-08-11 16:35:52 +0800

    提交数组处理 以及 day.js 处理

---
+++ code/dealArray.js
@@ -0,0 +1,175 @@
+class dealCurrentArray {
+    constructor() { }
+
+    //  判断是否为数组对象
+    // 参数一 判定对象
+    judgeArray(array) {
+        return Array.isArray(array)
+    }
+
+    // 非对象数组去重
+    removeSameArray(array) {
+        return [...new Set(array)]
+    }
+
+    // 对象数组去重
+    removeSameObjectArray(array) {
+        return [...new Set(array.map(i => JSON.stringify(i)))].map(j => JSON.parse(j))
+    }
+
+    // 选择数组的前几项
+    // 参数一：需要处理的数组 参数二：需要选择多少项
+    spliceArrayPreItems(array, num) {
+        array.splice(1, num)
+        return array
+    }
+
+    // 数组合并
+    mergeArray(array1, array2) {
+        return [...array1, ...array2]
+    }
+
+    // 输出两个数组公共部分（交集） 
+    arrayIntersection(array1, array2) {
+        return [...new Set(array1)].filter((item) => array2.includes(item));
+    }
+
+    // 删除数组中所有对象包含的一项
+    // 参数一 原数组 参数2 删除中包含的项的key 
+    deleteArrayObjectElement(array, elementKey) {
+        let newObj = {}
+        let newArr = []
+        array.forEach(function (item) {
+            for (var key in item) {
+                if (key != elementKey) {
+                    newObj[key] = item[key];
+                }
+            }
+            newArr.push(newObj);
+            newObj = {};//这步至关重要，每循环一次，都要清空一次，否则拿到的数据总是最后一条
+        });
+        return newArr
+    }
+
+    // 删除数组中的一项
+    // 参数一 原数组 参数二 删除该项中的key 参数三 删除该项中key对应的value
+    deleteArrayElement(array, elementKey, elementValue) {
+        // array.map((item, index) => {
+        //     for (const key in item) {
+        //         if (key == elementKey && item[key] == elementValue) {
+        //             delete array[index]
+        //         }
+        //     }
+        // })
+        // let copy = array.filter(function (s) {
+        //     return s;
+        // });
+        // return copy
+        let newArray = new Set(array)
+        array.map(item => {
+            for (let key in item) {
+                key == elementKey && item[key] == elementValue ? newArray.delete(item) : ''
+            }
+        })
+        // return [...new Set(Array.from(newArray).map(i=>JSON.stringify(i)))].map(j=>JSON.parse(j)) //去重对象数组
+        return [...newArray]
+
+    }
+
+    // 更改数组的key值 
+    // 参数一 原数组 参数二 原本的key值 参数三 新的key 值
+    changeArrayKey(array, oldKey, newKey) {
+        let newArray = []
+        array.map(item => {
+            let _item = JSON.parse(JSON.stringify(item).replace(oldKey, newKey))
+            newArray.push(_item)
+        })
+        return newArray
+    }
+
+    // 数组中根据某一项数据进行排序
+    // 参数一 原数组 参数二 排序规则 (up 升序 down 降序)  参数三  依据哪项数据(如纯数据数组 此参数可以不传)
+    sortArray(array, rule, key) {
+        if (key) {
+            return array.sort((a, b) => {
+                return rule == 'up' ? a[key] - b[key] : b[key] - a[key]
+            })
+        } else {
+            return array.sort((a, b) => {
+                return rule == 'up' ? a - b : b - a
+            })
+        }
+    }
+
+    // 输出数组中的极值
+    // 参数一 原数组 参数二 极值规则 参数三  依据哪项数据(如纯数据数组 此参数可以不传)
+    // * 由于Math.max 是一组数据 而不是数组形式返回 而方法的apply相当于调用函数(指定数组替换函数的参数) 恰好apply传数组 满足下面情况
+    arrayPeak(array,rule,key) {
+        if (key) {
+            let newArray = array.map(item=>item[key])
+            return rule == 'big' ? Math.max.apply(Math, newArray) : Math.min.apply(Math, newArray)
+        } else {
+            return rule == 'big' ? Math.max.apply(Math, array) : Math.min.apply(Math, array)
+        }
+    }
+
+    // 过滤数组中的虚值 空值
+    // 参数一 原数组 参数二 过滤的虚值key（如不为对象数组 此参数可以不传）
+    //  * 可过滤 false null undefined ''  NaN 不含此项值
+    arrayFilterInvented(array, key) {
+        if (key) {
+            return array.filter(item=>item[key])
+        } else {
+            return array.filter(Boolean)
+        }
+    }
+
+    // 数组的拷贝
+    // 参数一 原数组 参数二 拷贝规则（deep 深拷贝 shadow 浅拷贝）
+    arrayClone(array, rule) {
+        if (rule == 'deep') {
+            return  JSON.parse(JSON.stringify(array))
+        } else {
+            return array.concat()
+        }
+    }
+}
+
+// 数组扁平化数据转成tree形数据 Json格式是通过 id 以及 parent_id进行关联的
+// 参数一 扁平化数据
+export function flatToTree(array) {
+    let treeList = array.reduce((prev, cur) => {
+        prev[cur['id']] = cur;
+        return prev
+    }, {})
+    // console.log(treeList)
+
+    //通过引用类型原理实现  
+    let result = array.reduce((prev, cur) => {
+        let pid = cur.parent_id;
+        // pid为0的就找不到父对象，找到当前cur的父对象
+        // 对象的浅拷贝，引用关系存在，在后面处理parent的时候也会导致cur的改变，达到递归的效果
+        let parent = treeList[pid]
+        // 如果父对象存在，就将cur压到父对象的children属性中
+        if (parent) {
+            // parent和cur存在引用关系
+            parent.children ? parent.children.push(cur) : parent.children = [cur]
+        } else if (pid === 0) {
+            // 没有父对象，则此cur为树的根元素
+            prev.push(cur)
+        }
+        return prev
+    }, [])
+    return result
+}
+
+// 数组tree形数据转成扁平化数据 子代放于children中
+// 由于此方法涉及递归 故不适合封装于类中  
+export function treeToFlat(treeData) {
+    return treeData.reduce((res, item) => {
+            const { children, ...i } = item
+        return res.concat(i, children && children.length ? treeToFlat(children) : [])
+        }, [])
+    }
+
+export default dealCurrentArray 
---
+++ code/time.js
@@ -0,0 +1,95 @@
+
+// npm install dayjs --save
+const dayJs = require("dayjs");
+let arrUnit = [
+    { type: 'year', value: '年' },
+    { type: 'month', value: '月' },
+    { type: 'day', value: '天' },
+    { type: 'week', value: '周' },
+    { type: 'hour', value: '时' },
+    { type: 'minute', value: '分' },
+    { type: 'second', value: '秒' },
+    { type: 'millisecond', value: '：毫秒' }
+]
+class timeFormatter {
+    constructor() {
+    }
+    // 处理时间 默认返回当前时间精确到秒  参数{ format：需要处理的格式(默认 'YYYY-MM-DD HH:mm:ss') time:需要处理的时间（默认当前时间）}
+    dealCurrentTime(format,time) {
+        // 默认精确到秒 默认当前时间
+        if (format) {
+            return dayJs(time).format(format)
+        } else{
+            return dayJs(time).format('YYYY-MM-DD HH:mm:ss')
+        }
+    }
+    // 输出当前日期为周几 参数{time:默认当前日期}
+    getDayOfWeek(time) {
+        let DayOfWeek = dayJs(time).day()
+        console.log(DayOfWeek)
+        if (DayOfWeek == 1) {
+            return '星期一'
+        } else if (DayOfWeek == 2) {
+            return '星期二'
+        } else if (DayOfWeek == 3) {
+            return '星期三'
+        } else if (DayOfWeek == 4) {
+            return '星期四'
+        } else if (DayOfWeek == 5) {
+            return '星期五'
+        } else if (DayOfWeek == 6) {
+            return '星期六'
+        } else {
+            return '星期日'
+        }
+        
+    }
+    // 判断是否是闰年
+    getCurrentIsLeapYear(year) {
+        // return dayJs(year).isLeapYear()
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? '平年' : '闰年'
+    }
+
+    // 时间转时间戳
+    getTimeToKey(time) {
+        return dayJs(time).valueOf()
+    }
+
+    // 时间戳转时间
+    getKeyToTime(key) {
+        return dayJs(key).format('YYYY-MM-DD HH:mm:ss')
+    }
+
+//     加上或减去指定时间 
+//     参数{ 
+//     time: 基准时间 ；
+//     addOrSubtract: 'add':加 ， 'subtract': 减
+//     length：Number 需要加多久;
+//     unit：跨越单位 'week'：周  'day'：日 'month': 月（0 - 11） 'year'：年 'hour'：时 'minute':分 'second':秒 'millisecond'：毫秒 (不区分大小写 )
+//        }
+    getNewDateFromNow(time, addOrSubtract, length, unit) {
+        if (addOrSubtract == 'add') {
+            return dayJs(time).add(length, unit).format('YYYY-MM-DD HH:mm:ss')
+        } else if (addOrSubtract == 'subtract') {
+            return dayJs(time).subtract(length, unit).format('YYYY-MM-DD HH:mm:ss')
+        } else {
+            return  '参数错误'
+        }
+    }
+
+    // 计算两个时间的间距
+    // arr 是需要计算的两个时间 数组传递
+    // unit：跨越单位 'week'：周  'day'：日 'month': 月（0 - 11） 'year'：年 'hour'：时 'minute':分 'second':秒 'millisecond'：毫秒 (不区分大小写 )
+    // 结果保留两位
+    getTwoDateSpacing(arr, unit) {
+        let newUnit = arrUnit.filter(item => item.type == unit)
+        let newArr = arr.map(item => dayJs(item))
+        return newArr[0].diff(newArr[1], unit).toFixed(2) > 0 ? '早' : '晚' +  Math.abs(newArr[0].diff(newArr[1], unit).toFixed(2))  +  newUnit[0].value 
+    }
+
+    // 计算当前月份的天数
+    getCurrentMonthDays(time) {
+        return dayJs(time).daysInMonth() 
+    }
+}
+export default timeFormatter
