51768fdf32526966c4a4ef4b4c477449b15a5cc0
SmallMonkey-ai <2641973190@qq.com>
2022-08-17 14:51:57 +0800

    完善提交对象方法详情页

--- vue3/fromVite/src/components/dealObject.vue
+++ vue3/fromVite/src/components/dealObject.vue
@@ -1,34 +1,40 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed,getCurrentInstance } from 'vue';
 const tableData = [
     {
         wayName: 'judgeObject',
         params: 'object',
+        paramsOption:'-',
         desc: ' 判断只是对象（不是数组） ',
     },
     {
         wayName: 'judgeObjectIsEmpty',
         params: 'object',
+        paramsOption: '-',
         desc: '判断当前对象是否为空',
     },
     {
         wayName: 'ergodicObjectIsInclude',
-        params: 'object key',
+        params: 'object || key',
+        paramsOption:'-',
         desc: '遍历对象查看是否包括某key项值',
     },
     {
         wayName: 'mergeObject',
         params: 'object',
+        paramsOption:'-',
         desc: '对象合并 将需要合并的对象以数组传入 [obj1,obj2,obj3] 当只传一个对象是 相当于对象的浅拷贝',
     },
     {
         wayName: 'removeObjectEmpty',
         params: 'object',
+        paramsOption: '-',
         desc: '去除对象里面的空值',
     },
     {
         wayName: 'changeObjectKey',
-        params: 'object newkeyS',
+        params: 'object || newkeyS',
+        paramsOption:'-',
         desc: '更改对象里的key值',
     }
 ]
@@ -40,8 +46,9 @@ const filterTableData = computed(() =>
             data.desc.toLowerCase().includes(search.value.toLowerCase())
     )
 )
+let instance = getCurrentInstance()
 function showDetail(row) {
-    console.log(row)
+    instance?.proxy?.$router.push({ name: '方法详情', params: { wayName: row.wayName, params: row.params, paramsOption: row.paramsOption, desc: row.desc, id: 3 } })
 }
 </script>
 
@@ -50,7 +57,8 @@ function showDetail(row) {
         <h1>欢迎来到对象处理页</h1>
         <el-table :data="filterTableData" height="650" style="width: 100%" @row-click="showDetail">
             <el-table-column prop="wayName" label="方法名" width="180" />
-            <el-table-column prop="params" label="参数" width="240" />
+            <el-table-column prop="params" label="必传参数" width="240" />
+            <el-table-column prop="paramsOption" label="可选参数" width="240" />
             <el-table-column prop="desc" label="描述" />
             <el-table-column align="right">
                 <template #header>
